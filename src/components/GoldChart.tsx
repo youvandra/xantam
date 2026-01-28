f4a5f8f5621ff77f3cd17eebb9da9185f255308b
Youvandra Febrial <kucingplaygame@gmail.com>
2026-01-29 05:48:07 +0700

    feat(trade): replace market depth with gold price chart

---
+++ src/components/GoldChart.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef, memo } from 'react';
+
+function GoldChart() {
+  const container = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (container.current && !container.current.querySelector("script")) {
+      const script = document.createElement("script");
+      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+      script.type = "text/javascript";
+      script.async = true;
+      script.innerHTML = `
+        {
+          "autosize": true,
+          "symbol": "(OANDA:XAUUSD*FX_IDC:USDIDR)/31.1035",
+          "interval": "D",
+          "timezone": "Etc/UTC",
+          "theme": "light",
+          "style": "1",
+          "locale": "en",
+          "enable_publishing": false,
+          "allow_symbol_change": true,
+          "calendar": false,
+          "support_host": "https://www.tradingview.com"
+        }`;
+      container.current.appendChild(script);
+    }
+  }, []);
+
+  return (
+    <div className="tradingview-widget-container h-full w-full bg-white rounded-2xl overflow-hidden shadow-sm border border-gray-100 p-4" ref={container}>
+      <div className="tradingview-widget-container__widget" style={{ height: "100%", width: "100%" }}></div>
+    </div>
+  );
+}
+
+export default memo(GoldChart);
--- src/pages/Trade.tsx
+++ src/pages/Trade.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import SwapCard from '../components/SwapCard';
-import MarketDepth from '../components/MarketDepth';
+import GoldChart from '../components/GoldChart';
 
 export default function Trade() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 items-start max-w-6xl mx-auto">
-      <div className="lg:col-span-7 lg:sticky lg:top-24">
-        {/* Just a placeholder for the left area - could be charts, info, etc */}
-        <div className="aspect-[4/3] w-full">
-           <MarketDepth />
+    <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 items-start max-w-7xl mx-auto">
+      <div className="lg:col-span-8 lg:sticky lg:top-24">
+        {/* TradingView Chart */}
+        <div className="w-full h-[600px]">
+           <GoldChart />
         </div>
       </div>
       
-      <div className="lg:col-span-5">
+      <div className="lg:col-span-4">
         <SwapCard />
       </div>
     </div>
